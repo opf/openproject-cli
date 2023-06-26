fefe21c6ccdbfc861ca4b74af394264700bc0baa
Andreas Pfohl <a.pfohl@openproject.com>
2023-06-26 16:32:12 +0200

    Refactored project ID parameter to be a mandatory flag

--- cmd/create/create.go
+++ cmd/create/create.go
@@ -9,5 +9,22 @@ var RootCmd = &cobra.Command{
 }
 
 func init() {
+	createWorkPackageCmd.Flags().Int64VarP(
+		&projectId,
+		"project",
+		"p",
+		0,
+		"Project ID to create the work package in",
+	)
+	_ = createWorkPackageCmd.MarkFlagRequired("project")
+
+	createWorkPackageCmd.Flags().BoolVarP(
+		&shouldOpenWorkPackageInBrowser,
+		"open",
+		"o",
+		false,
+		"Open the created work package in the default browser",
+	)
+
 	RootCmd.AddCommand(createWorkPackageCmd)
 }
--- cmd/create/work_package.go
+++ cmd/create/work_package.go
@@ -2,34 +2,40 @@ package create
 
 import (
 	"fmt"
+	"github.com/opf/openproject-cli/components/launch"
 	"github.com/opf/openproject-cli/components/resources/work_packages"
-	"strconv"
+	"github.com/opf/openproject-cli/components/routes"
 
 	"github.com/spf13/cobra"
 
 	"github.com/opf/openproject-cli/components/printer"
 )
 
+var projectId int64
+var shouldOpenWorkPackageInBrowser bool
+
 var createWorkPackageCmd = &cobra.Command{
-	Use:   "workpackage [project_id] [subject]",
+	Use:   "workpackage [subject]",
 	Short: "Create work package in project",
-	Long:  "Create a new work package with the given subject in a project selected by a project's ID",
+	Long:  "Create a new work package with the given subject in a project",
 	Run:   createWorkPackage,
 }
 
 func createWorkPackage(_ *cobra.Command, args []string) {
-	if len(args) != 2 {
-		printer.ErrorText(fmt.Sprintf("Expected 2 arguments [project_id] and [subject], but got %d", len(args)))
+	if len(args) != 1 {
+		printer.ErrorText(fmt.Sprintf("Expected 1 argument [subject], but got %d", len(args)))
 	}
 
-	id, err := strconv.ParseInt(args[0], 10, 64)
-	if err != nil {
-		printer.ErrorText(fmt.Sprintf("'%s' is an invalid project id. Must be a number.", args[0]))
+	subject := args[0]
+
+	workPackage := work_packages.Create(projectId, subject)
+
+	if shouldOpenWorkPackageInBrowser {
+		err := launch.Browser(routes.WorkPackageUrl(workPackage))
+		if err != nil {
+			printer.ErrorText(fmt.Sprintf("Error opening browser: %s", err))
+		}
+	} else {
+		printer.WorkPackage(workPackage)
 	}
-
-	subject := args[1]
-
-	workPackage := work_packages.Create(id, subject)
-
-	printer.WorkPackage(workPackage)
 }
