ba468ac95172b83ea908eb6e4357de65a193aaea
Eric Schubert <e.schubert@openproject.com>
2023-06-27 14:12:57 +0200

    Added type printer unit test

--- components/printer/types.go
+++ components/printer/types.go
@@ -2,17 +2,30 @@ package printer
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/opf/openproject-cli/components/common"
 	"github.com/opf/openproject-cli/models"
 )
 
 func Types(types []*models.Type) {
+	var maxIdLength = 0
 	for _, t := range types {
-		printType(t)
+		maxIdLength = common.Max(maxIdLength, idLength(t.Id))
+	}
+
+	for _, t := range types {
+		printType(t, maxIdLength)
 	}
 }
 
-func printType(workPackageType *models.Type) {
-	id := fmt.Sprintf("#%d", workPackageType.Id)
-	activePrinter.Printf("%s %s\n", Red(id), Yellow(workPackageType.Name))
+func printType(workPackageType *models.Type, maxIdLength int) {
+	var parts []string
+
+	diff := maxIdLength - idLength(workPackageType.Id)
+	idStr := fmt.Sprintf("%s#%d", indent(diff), workPackageType.Id)
+	parts = append(parts, Red(idStr))
+
+	parts = append(parts, Yellow(workPackageType.Name))
+	activePrinter.Println(strings.Join(parts, " "))
 }
---
+++ components/printer/types_test.go
@@ -0,0 +1,28 @@
+package printer_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/opf/openproject-cli/components/printer"
+	"github.com/opf/openproject-cli/models"
+)
+
+func TestTypes(t *testing.T) {
+	testingPrinter.Reset()
+
+	types := []*models.Type{
+		{Id: 42, Name: "Lightsaber"},
+		{Id: 1337, Name: "Leet"},
+	}
+
+	var expected string
+	expected += fmt.Sprintf("%s %s\n", printer.Red(fmt.Sprintf("  #%d", types[0].Id)), printer.Yellow(types[0].Name))
+	expected += fmt.Sprintf("%s %s\n", printer.Red(fmt.Sprintf("#%d", types[1].Id)), printer.Yellow(types[1].Name))
+
+	printer.Types(types)
+
+	if testingPrinter.Result != expected {
+		t.Errorf("Expected \n%s\tbut got \n%s", expected, testingPrinter.Result)
+	}
+}
