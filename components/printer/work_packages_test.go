7c84e9bdbe54fb9ad03a1547e7a7639c8c4f5878
Andreas Pfohl <a.pfohl@openproject.com>
2023-06-27 13:08:31 +0200

    Added test for printing multiple work packages

--- components/printer/work_packages_test.go
+++ components/printer/work_packages_test.go
@@ -2,23 +2,13 @@ package printer_test
 
 import (
 	"fmt"
-	"github.com/opf/openproject-cli/components/routes"
 	"strconv"
 	"testing"
 
 	"github.com/opf/openproject-cli/components/printer"
+	"github.com/opf/openproject-cli/components/routes"
 	"github.com/opf/openproject-cli/models"
 )
-
-//#8 TASK Setup conference website
-//[In progress]
-//Assignee: OpenProject Admin
-//
-//Open: https://openproject.local/work_packages/8
-//
-//# Awesome
-//
-//**Bacon ipsum** _d
 
 func TestWorkPackage(t *testing.T) {
 	testingPrinter.Reset()
@@ -46,5 +36,43 @@ func TestWorkPackage(t *testing.T) {
 
 	if testingPrinter.Result != expected {
 		t.Errorf("Expected %s, but got %s", expected, testingPrinter.Result)
+	}
+}
+
+func TestWorkPackages(t *testing.T) {
+	testingPrinter.Reset()
+
+	workPackages := []*models.WorkPackage{
+		{
+			Id:          42,
+			Subject:     "Test 1",
+			Type:        "PHASE",
+			Assignee:    models.Principal{Name: "Obi-Wan"},
+			Status:      "In progress",
+			Description: "This is one example.",
+			LockVersion: 0,
+		},
+		{
+			Id:          43,
+			Subject:     "Test 2",
+			Type:        "TASK",
+			Assignee:    models.Principal{Name: "Anakin"},
+			Status:      "New",
+			Description: "This is another example.",
+			LockVersion: 0,
+		},
+	}
+
+	var expected string
+
+	idString := "#" + strconv.FormatUint(workPackages[0].Id, 10)
+	expected += fmt.Sprintf("%s %s [%s] %s\n", printer.Red(idString), printer.Green(workPackages[0].Type), printer.Yellow(workPackages[0].Status), printer.Cyan(workPackages[0].Subject))
+	idString = "#" + strconv.FormatUint(workPackages[1].Id, 10)
+	expected += fmt.Sprintf("%s %s [%s]         %s\n", printer.Red(idString), printer.Green(workPackages[1].Type+" "), printer.Yellow(workPackages[1].Status), printer.Cyan(workPackages[1].Subject))
+
+	printer.WorkPackages(workPackages)
+
+	if testingPrinter.Result != expected {
+		t.Errorf("\nExpected:\n%sbut got:\n%s", expected, testingPrinter.Result)
 	}
 }
