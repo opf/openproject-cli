e6196cd477adc7e57ed9af3dc1ceb29f31b4a56e
Andreas Pfohl <a.pfohl@openproject.com>
2023-06-27 13:28:48 +0200

    Added test for printing notifications

---
+++ components/printer/notifications_test.go
@@ -0,0 +1,49 @@
+package printer_test
+
+import (
+	"fmt"
+	"github.com/opf/openproject-cli/components/common"
+	"github.com/opf/openproject-cli/components/printer"
+	"github.com/opf/openproject-cli/models"
+	"strconv"
+	"testing"
+)
+
+func TestNotifications(t *testing.T) {
+	testingPrinter.Reset()
+
+	notifications := []*models.Notification{
+		{
+			Id:              42,
+			ResourceId:      15,
+			ResourceSubject: "Subject 1",
+			Reason:          "Comment created",
+			Read:            false,
+			CreatedAt:       "",
+			UpdatedAt:       "",
+		},
+		{
+			Id:              54,
+			ResourceId:      12,
+			ResourceSubject: "Subject 2",
+			Reason:          "Status changed",
+			Read:            false,
+			CreatedAt:       "",
+			UpdatedAt:       "",
+		},
+	}
+
+	expected := common.Reduce[*models.Notification, string](
+		notifications,
+		func(state string, notification *models.Notification) string {
+			idString := "#" + strconv.FormatUint(notification.ResourceId, 10)
+			return state + fmt.Sprintf("[%s] %s (%s)\n", printer.Red(idString), printer.Cyan(notification.ResourceSubject), notification.Reason)
+		},
+		"")
+
+	printer.Notifications(notifications)
+
+	if testingPrinter.Result != expected {
+		t.Errorf("\nExpected:\n%sbut got:\n%s", expected, testingPrinter.Result)
+	}
+}
