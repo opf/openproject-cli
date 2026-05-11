e5bd97fde6ba18247041ec33209f0ab2c1e16a36
David <david@ygland.se>
2026-05-11 15:03:55 +0200

    added flags functionality to login

--- cmd/login.go
+++ cmd/login.go
@@ -26,9 +26,15 @@ var loginCmd = &cobra.Command{
 	Long: `Enables the login flow, which enables the user to use
 this tool for a specific OpenProject instance. The login
 needs the host URL of the OpenProject instance and a
-generated API token.`,
+generated API token. You can provide both with flags to
+skip interactive input.`,
 	Run: login,
 }
+
+var (
+	loginHostFlag   string
+	loginAPIKeyFlag string
+)
 
 const (
 	urlInputError      = "There was a problem parsing the input. Please try again and put in a valid URL."
@@ -41,51 +47,84 @@ func login(_ *cobra.Command, _ []string) {
 	var hostUrl *url.URL
 	var token string
 
-	for {
-		printer.Debug(Verbose, "Parsing host URL ...")
-		printer.Input("OpenProject host URL: ")
-
-		ok, msg, host := parseHostUrl()
+	if loginHostFlag != "" {
+		printer.Debug(Verbose, "Parsing host URL from --host ...")
+		ok, msg, host := parseHostUrlInput(loginHostFlag)
 		if !ok {
 			printer.ErrorText(msg)
-			continue
+			return
 		}
 
 		printer.Debug(Verbose, "Initializing requests client ...")
 		requests.Init(host, "", Verbose)
-		ok = checkOpenProjectApi()
-		if !ok {
+		if !checkOpenProjectApi() {
 			printer.ErrorText(noOpInstanceError)
-			continue
+			return
 		}
 
 		hostUrl = host
-		break
+	} else {
+		for {
+			printer.Debug(Verbose, "Parsing host URL ...")
+			printer.Input("OpenProject host URL: ")
+
+			ok, msg, host := parseHostUrl()
+			if !ok {
+				printer.ErrorText(msg)
+				continue
+			}
+
+			printer.Debug(Verbose, "Initializing requests client ...")
+			requests.Init(host, "", Verbose)
+			ok = checkOpenProjectApi()
+			if !ok {
+				printer.ErrorText(noOpInstanceError)
+				continue
+			}
+
+			hostUrl = host
+			break
+		}
 	}
 
-	for {
-		fmt.Printf("OpenProject API Token (Visit %s/my/access_tokens to generate one): ", hostUrl)
-		ok, t := requestApiToken()
-		if !ok {
-			fmt.Println(tokenInputError)
-			continue
-		}
-
-		token = common.SanitizeLineBreaks(t)
-
+	if loginAPIKeyFlag != "" {
+		token = common.SanitizeLineBreaks(loginAPIKeyFlag)
 		requests.Init(hostUrl, token, Verbose)
 		user, err := users.Me()
 		if err != nil {
 			printer.Error(err)
-			continue
+			return
 		}
 
 		if user.Name == "Anonymous" {
 			printer.ErrorText("no authenticate given")
-			continue
+			return
 		}
+	} else {
+		for {
+			fmt.Printf("OpenProject API Token (Visit %s/my/access_tokens to generate one): ", hostUrl)
+			ok, t := requestApiToken()
+			if !ok {
+				fmt.Println(tokenInputError)
+				continue
+			}
 
-		break
+			token = common.SanitizeLineBreaks(t)
+
+			requests.Init(hostUrl, token, Verbose)
+			user, err := users.Me()
+			if err != nil {
+				printer.Error(err)
+				continue
+			}
+
+			if user.Name == "Anonymous" {
+				printer.ErrorText("no authenticate given")
+				continue
+			}
+
+			break
+		}
 	}
 
 	storeLoginData(hostUrl, token)
@@ -99,6 +138,11 @@ func parseHostUrl() (ok bool, errMessage string, host *url.URL) {
 		printer.Debug(Verbose, fmt.Sprintf("Error reading string input: %+v", err))
 		return false, urlInputError, nil
 	}
+
+	return parseHostUrlInput(input)
+}
+
+func parseHostUrlInput(input string) (ok bool, errMessage string, host *url.URL) {
 
 	printer.Debug(Verbose, fmt.Sprintf("Parsed input %q.", input))
 	printer.Debug(Verbose, "Sanitizing input ...")
@@ -124,6 +168,22 @@ func parseHostUrl() (ok bool, errMessage string, host *url.URL) {
 
 	printer.Debug(Verbose, "Parsing input successful, continuing with next steps.")
 	return true, "", parsed
+}
+
+func init() {
+	loginCmd.Flags().StringVar(
+		&loginHostFlag,
+		"host",
+		"",
+		"OpenProject host URL",
+	)
+
+	loginCmd.Flags().StringVar(
+		&loginAPIKeyFlag,
+		"api-key",
+		"",
+		"OpenProject API token",
+	)
 }
 
 func checkOpenProjectApi() bool {
