3f77c2e3315ea80c2cae02b73624df8ab58ebcd9
Alexander Brandon Coles <a.coles@openproject.com>
2026-07-25 22:18:00 +0200

    [#74415] add schema fetch and field resolution
    Schema-driven custom field resolution: label or API-name lookup,
    writability check, and type coercion for String, Formattable, Integer,
    Float, Boolean, and Date fields.
    
    https://community.openproject.org/wp/CLI-12

---
+++ components/resources/work_packages/schema.go
@@ -0,0 +1,56 @@
+package work_packages
+
+import (
+	"sort"
+
+	"github.com/opf/openproject-cli/components/parser"
+	"github.com/opf/openproject-cli/components/requests"
+	"github.com/opf/openproject-cli/dtos"
+)
+
+type SchemaField struct {
+	APIName  string
+	Label    string
+	Type     string
+	Writable bool
+}
+
+type Schema struct {
+	Fields []SchemaField
+}
+
+func SchemaFor(workPackage *dtos.WorkPackageDto) (*Schema, error) {
+	if workPackage == nil || workPackage.Links == nil || workPackage.Links.Schema == nil {
+		return &Schema{}, nil
+	}
+
+	response, err := requests.Get(workPackage.Links.Schema.Href, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	dto := parser.Parse[dtos.WorkPackageSchemaDto](response)
+	fields := make([]SchemaField, 0, len(dto.Fields))
+	for apiName, field := range dto.Fields {
+		fields = append(fields, SchemaField{
+			APIName:  apiName,
+			Label:    field.Name,
+			Type:     field.Type,
+			Writable: field.Writable,
+		})
+	}
+
+	sort.Slice(fields, func(i, j int) bool {
+		return fields[i].APIName < fields[j].APIName
+	})
+
+	return &Schema{Fields: fields}, nil
+}
+
+func (schema *Schema) fieldLabels() map[string][]string {
+	labels := make(map[string][]string)
+	for _, field := range schema.Fields {
+		labels[field.Label] = append(labels[field.Label], field.APIName)
+	}
+	return labels
+}
